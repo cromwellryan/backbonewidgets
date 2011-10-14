8cffa2b0e3a1a08e483b1e6400943444909f90b7
Ryan Cromwell <ryan@echelontouch.com>
2011-10-14 15:36:40 -0400

    removed cruft

--- todo.js
+++ todo.js
@@ -1,76 +1,60 @@
-    $(function () {
+$(function () {
+	var Todo = function (containerElement) {
+		var parentElement = containerElement;
+	};
 
-        var Todo = function (containerElement) {
-            var parentElement = containerElement;
-        };
-        
-        Todo.prototype = function () {
-            
-            TodoModel = Backbone.Model.extend({
-                initialize: function() {
-                    this.bind("change", this.onChange);
-                }
-                , onChange: function() {
-                }
-            }),
-            TodoStore = Backbone.Collection.extend({
-                initialize: function() { alert('store'); },
-                model: this.TodoModel,
-                url: '/todo'
-            }),
+	Todo.prototype = function () {
+			
+		TodoModel = Backbone.Model.extend({}),
+			
+		TodoEntry = Backbone.View.extend({
+					initialize: function () {
+							var me = this;
+							$.get(".\item.htm", function(content) {
+									$(me.el).append(content);
+									$(me.options.container).append(me.el);
 
-            TodoEntry = Backbone.View.extend({
-                initialize: function () {
-                    var me = this;
-                    $.get("Assets/Todo/Item.html", function(content) {
-                        $(me.el).append(content);
-                        $(me.options.container).append(me.el);
+									Backbone.ModelBinding.bind(me);
+							});
+					}
+		}),
 
-                        Backbone.ModelBinding.bind(me);
-                    });
-                }
+		TodoList = Backbone.View.extend({
+			initialize: function () { this.collection.bind("all", this.render); },
+			render: function () {
+				var table = $(".todo-table", this.parentElement);
+				table.empty();
+				if (this.models == null) return;
 
-                ,onChange: function() { 
-                }
-            }),
+				this.models.forEach(function (todo) {
+					var zoneEntry = new TodoEntry({ model: todo, container: table });
+				});
 
-            TodoList = Backbone.View.extend({
-                initialize: function () { this.collection.bind("all", this.render); },
-                render: function () {
-                    var table = $(".todo-table", this.parentElement);
-                    table.empty();
-                    if (this.models == null) return;
-                    this.models.forEach(function (todo) {
-                        var zoneEntry = new TodoEntry({ model: todo, container: table });
+				return this;
+			}
+		}),
 
-                    });
-                    return this;
-                }
-            }),
+		TodoRouter = Backbone.Router.extend({
+			routes: {
+				"": "list",
+			}
+			,list: function () {
+				var todos = new Backbone.Collection([ {Title: "Brush Teeth"}, 
+					{Title: "Sweep"}, 
+					{Title: "Wash Face"}] );
+				var view = new TodoList({ collection: todos });
+			}
+		});
 
-            TodoRouter = Backbone.Router.extend({
-                initialize: function() { this.todoList = new TodoStore(); }
-                ,routes: {
-                    "": "list",
-                    "new": "newtodo"
-                }
-                ,list: function () {
-                    var view = new TodoList({ collection: this.todoList });
-                    this.todoList.fetch();
-                }
-                ,newtodo: function() {
-                    var view = new TodoEdit({model: this.todoList.create() });
-                }
-            });
+			return { start: function() {new TodoRouter()} };
+	} ();
 
-            return { start: function() {new TodoRouter()} };
-        } ();
+	$(".todo-list").each( function(index, element) {
+		var todo = new Todo(element);
+		todo.start();
+	});
 
-        $(".todo-list").each( function(index, element) {
-            var todo = new Todo(element);
-            todo.start();
-            });
+	Backbone.history.start();
 
-        Backbone.history.start();
-    });
-    
+});
+
